fb75913e49ef6208b2925a3c8dcc533437c55b7f
LilithRelish <laragoblin@gmail.com>
2020-10-29 14:40:22 +0100

    borrado ok

--- borrar.php
+++ borrar.php
@@ -25,17 +25,8 @@ if(isset($_REQUEST['borrar'])){
     $num = $_REQUEST['num_incidencia'];
     foreach($_SESSION['incidencias'] as $clave=>$valor){
         if($valor[0]==$num){
-            echo count($_SESSION['incidencias']);
-            echo 'Eh, el numero funciona, es: '.$valor[0].'<br>';
-            echo $valor[1].'<br>';
-            echo $valor[2].'<br>';
-            echo 'Esto deberia ser el numero a borrar '.$clave;
             unset($_SESSION['incidencias'][$clave]);
-            echo count($_SESSION['incidencias']);
-        }
-            
-        
-        
+        }   
     }
     
     //echo '<br>';
