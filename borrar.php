8543e853c238fef27c1039321438dfd58269c5d6
LilithRelish <laragoblin@gmail.com>
2020-10-29 14:33:38 +0100

    valores?

--- borrar.php
+++ borrar.php
@@ -25,8 +25,9 @@ if(isset($_REQUEST['borrar'])){
     $num = $_REQUEST['num_incidencia'];
     foreach($_SESSION['incidencias'] as $clave=>$valor){
         if($valor[0]==$num){
-            echo $clave.'<br>';
-            echo $valor;
+            echo 'Eh, el numero funciona, es: '.$valor[0].'<br>';
+            echo $valor[1].'<br>';
+            echo $valor[2];
             //unset($_SESSION['incidencias'][$clave]);
         }
             
