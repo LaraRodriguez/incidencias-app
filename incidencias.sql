1f0deb7de53ae98794f3fc3943a0b3552924b1d0
LilithRelish <laragoblin@gmail.com>
2020-11-04 15:01:44 +0100

    table

--- incidencias.sql
+++ incidencias.sql
@@ -5,8 +5,12 @@ CREATE TABLE incidencia(
  numero tinyint(3) unsigned not null auto_increment primary key,
  urgencia varchar(80) not null,
  tipo varchar(80) not null,
- fechahora int not null,
+ fechahora varchar(80) not null,
  lugar varchar(80) not null,
- ip int not null,
+ ip varchar(80) not null,
  descripcion varchar(120)
 )CHARSET=utf8 COLLATE=utf8_general_ci;
+INSERT INTO incidencia(numero, urgencia, tipo, fechahora, lugar, ip, descripcion)
+VALUES
+(1,"Si","Vandalismo","25-10-2020 10:37:43","Plaza mayor","193.12.56.25","Farola rota"),
+(2,"Si","Basuras","26-10-2020 12:07:23","C/Pelicano 25","131.120.121.125","Contenedor basura roto");
--- index.php
+++ index.php
@@ -19,6 +19,37 @@ $incidencias[]=array(2,"Si","Basuras","26-10-2020 12:07:23","C/Pelicano 25","131
 $incidencias[]=array(3,"Si","Vandalismo","27-10-2020 00:07:23","C/Cigueña 5","145.10.11.15","Banco roto");
 $_SESSION['incidencias'] = $incidencias;*/
 
+$sql="SELECT * FROM incidencia ORDER BY numero";  
+  $consulta = $con->prepare($sql);
+  $consulta->execute();
+  $registros = $consulta->fetchAll();
+   print "<BR> <br>TODOS LOS REGISTROS<br>"; 
+   print'<table border="2">
+		<tr>
+		<th>numero</th>
+		<th>urgencia</th>
+        <th>tipo</th>
+        <th>fecha</th>
+        <th>lugar</th>
+        <th>ip</th>
+        <th>descripcion</th>
+		</tr>';
+  foreach($registros as $item)
+  {
+      echo '<tr><td>'.$item[0] .'</td>'	;	
+      //ó $item['nombre']
+      echo '<td>'.$item[1] .'</td>'	;
+      echo '<td>'.$item[2] .'</td>';
+      echo '<td>'.$item[2] .'</td>';
+      echo '<td>'.$item[2] .'</td>';
+      echo '<td>'.$item[2] .'</td>';
+      echo '<td>'.$item[2] .'</td></tr>';
+      //ó $item['descripcion']
+		
+      
+  }
+print'</table>';
+
 
 
 include 'pie.php';
